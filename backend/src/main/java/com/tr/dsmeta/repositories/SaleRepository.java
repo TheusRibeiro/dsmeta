4ca7ddbe1f87f2a40ec1c41c7be31aa7dd38cbff
theusribeiro <matheusohn@gmail.com>
2022-09-27 23:36:09 -0300

    API test

---
+++ backend/src/main/java/com/tr/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.tr.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tr.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
